a1a783acfcc59d4263c0114df02acbe2e66844b7
mjamil <mohamadmaherjamil@gmail.com>
2024-09-12 17:07:38 +0300

    addecho

--- src/builtins/echo.c
+++ src/builtins/echo.c
@@ -5,45 +5,52 @@
 # include <string.h>
 # include <stdbool.h>
 
-bool check_quotes(const char *str)
+bool is_n_flag(const char *arg)
 {
-    int i;
-    bool inside_quote = false;
-
-    i = 0;
-    while (i < strlen(str))
+    if (arg[0] == '-')
     {
-        if (str[i] == '"')
-            inside_quote = !inside_quote;
-        i++;
+        for (int i = 1; arg[i] != '\0'; i++)
+        {
+            if (arg[i] != 'n')
+                return false;
+        }
+        return true;
     }
-    return !inside_quote;
+    return false;
 }
 
-void echo(const char *message)
+void echo(const char *message, bool no_newline)
 {
-    if (check_quotes(message))
-        printf("%s\n", message);
+    printf("%s", message);
+    if (!no_newline)
+        printf("\n");
 }
 
 int main(int argc, char *argv[])
 {
-    if (argc > 1)
+    if (argc > 1 && strcmp(argv[1], "echo") == 0)
     {
-        char message[1024] = "";
+        bool no_newline = false;
         int i = 2;
-        while (i < argc)
+        while (i < argc && is_n_flag(argv[i]))
         {
-            if (i > 2)
-            {
-                strcat(message, " ");
-            }
-            strcat(message, argv[i]);
+            no_newline = true;
             i++;
         }
-        echo(message);
+        char message[1024] = "";
+        bool first_word = true;
+        while (i < argc)
+        {
+            if (!first_word)
+                strcat(message, " ");
+            strcat(message, argv[i]);
+            first_word = false;
+            i++;
+        }
+        echo(message, no_newline);
     }
     else
         write(1, "\n", 1);
+
     return 0;
 }
