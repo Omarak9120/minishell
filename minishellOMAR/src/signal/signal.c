8d0c3926cc2da49d16a822580c8e507a91151d47
omarak9120 <omarak@omar.localdomain>
2024-12-29 21:07:43 +0200

    omar

--- minishellOMAR/src/signal/signal.c
+++ minishellOMAR/src/signal/signal.c
@@ -12,29 +12,6 @@
 
 #include "../../includes/minishell.h"
 
-// char	*getinfo(t_data *data)
-// {
-// 	char	*info;
-// 	char	*sig;
-// 	char	*pwd;
-
-// 	pwd = get_env(data->env_list, "PWD");
-// 	sig = ft_itoa(data->cmd.status);
-// 	info = ft_strdup("");
-// 	info = ft_strjoingnl(info, BLUE_BRIGHT);
-// 	info = ft_strjoingnl(info, pwd);
-// 	info = ft_strjoingnl(info, RESET);
-// 	info = ft_strjoingnl(info, BG_RED_BRIGHT);
-// 	info = ft_strjoingnl(info, sig);
-// 	info = ft_strjoingnl(info, RESET);
-// 	info = ft_strjoingnl(info, GREEN "⫤ " RESET);
-// 	if (sig)
-// 		free(sig);
-// 	if (pwd != NULL)
-// 		free(pwd);
-// 	return (info);
-// }
-
 char	*getinfo(t_data *data)
 {
 	char	*info;
@@ -43,28 +20,18 @@ char	*getinfo(t_data *data)
 
 	pwd = get_env(data->env_list, "PWD");
 	sig = ft_itoa(data->cmd.status);
-
 	info = ft_strdup("");
-	if (!info)
-		return (NULL);
-	/*
-	 * Example prompt design:
-	 *  [ /current/path : 0 ] ⫤ 
-	 *    - "/current/path" in BLUE_BRIGHT
-	 *    - Status in BG_RED_BRIGHT
-	 *    - Then the green arrow
-	 */
-	info = ft_strjoingnl(info, BLUE_BRIGHT "[ " RESET);
-	info = ft_strjoingnl(info, (pwd ? pwd : "unknown_dir"));
-	info = ft_strjoingnl(info, BLUE_BRIGHT " : " RESET);
+	info = ft_strjoingnl(info, BLUE_BRIGHT);
+	info = ft_strjoingnl(info, pwd);
+	info = ft_strjoingnl(info, RESET);
 	info = ft_strjoingnl(info, BG_RED_BRIGHT);
 	info = ft_strjoingnl(info, sig);
 	info = ft_strjoingnl(info, RESET);
-	info = ft_strjoingnl(info, BLUE_BRIGHT " ]" RESET " ");
-	info = ft_strjoingnl(info, GREEN ">> " RESET);
-
-	free(sig);
-	free(pwd);
+	info = ft_strjoingnl(info, GREEN "⫤ " RESET);
+	if (sig)
+		free(sig);
+	if (pwd != NULL)
+		free(pwd);
 	return (info);
 }
 
@@ -74,7 +41,6 @@ void	interactivemode(t_data *data, char **input)
 
 	signal(SIGINT, interactivehandle_sigint);
 	signal(SIGQUIT, interactivehandle_sigquit);
-	signal(SIGTSTP, SIG_IGN);
 	while (1)
 	{
 		info = getinfo(data);
@@ -99,7 +65,6 @@ void	noninteractivemode(t_data *data, char **input)
 {
 	signal(SIGINT, noninteractivehandle_sigint);
 	signal(SIGQUIT, noninteractivehandle_sigint);
-	signal(SIGTSTP, SIG_IGN);
 	while (data->cmd.running == 1 && g_signalint != 130)
 	{
 		if (g_signalint == 130)
--- minishellTest/src/signal/signal.c
+++ minishellTest/src/signal/signal.c
@@ -1,16 +1,27 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   signal.c                                           :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: zmourtab <zakariamourtaban@gmail.com>      +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/08/05 00:00:34 by zmourtab          #+#    #+#             */
-/*   Updated: 2024/09/04 18:00:11 by zmourtab         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "../../includes/minishell.h"
+
+// char	*getinfo(t_data *data)
+// {
+// 	char	*info;
+// 	char	*sig;
+// 	char	*pwd;
+
+// 	pwd = get_env(data->env_list, "PWD");
+// 	sig = ft_itoa(data->cmd.status);
+// 	info = ft_strdup("");
+// 	info = ft_strjoingnl(info, BLUE_BRIGHT);
+// 	info = ft_strjoingnl(info, pwd);
+// 	info = ft_strjoingnl(info, RESET);
+// 	info = ft_strjoingnl(info, BG_RED_BRIGHT);
+// 	info = ft_strjoingnl(info, sig);
+// 	info = ft_strjoingnl(info, RESET);
+// 	info = ft_strjoingnl(info, GREEN "⫤ " RESET);
+// 	if (sig)
+// 		free(sig);
+// 	if (pwd != NULL)
+// 		free(pwd);
+// 	return (info);
+// }
 
 char	*getinfo(t_data *data)
 {
@@ -20,18 +31,28 @@ char	*getinfo(t_data *data)
 
 	pwd = get_env(data->env_list, "PWD");
 	sig = ft_itoa(data->cmd.status);
+
 	info = ft_strdup("");
-	info = ft_strjoingnl(info, BLUE_BRIGHT);
-	info = ft_strjoingnl(info, pwd);
-	info = ft_strjoingnl(info, RESET);
+	if (!info)
+		return (NULL);
+	/*
+	 * Example prompt design:
+	 *  [ /current/path : 0 ] ⫤ 
+	 *    - "/current/path" in BLUE_BRIGHT
+	 *    - Status in BG_RED_BRIGHT
+	 *    - Then the green arrow
+	 */
+	info = ft_strjoingnl(info, BLUE_BRIGHT "[ " RESET);
+	info = ft_strjoingnl(info, (pwd ? pwd : "unknown_dir"));
+	info = ft_strjoingnl(info, BLUE_BRIGHT " : " RESET);
 	info = ft_strjoingnl(info, BG_RED_BRIGHT);
 	info = ft_strjoingnl(info, sig);
 	info = ft_strjoingnl(info, RESET);
-	info = ft_strjoingnl(info, GREEN "⫤ " RESET);
-	if (sig)
-		free(sig);
-	if (pwd != NULL)
-		free(pwd);
+	info = ft_strjoingnl(info, BLUE_BRIGHT " ]" RESET " ");
+	info = ft_strjoingnl(info, GREEN ">> " RESET);
+
+	free(sig);
+	free(pwd);
 	return (info);
 }
 
@@ -41,6 +62,7 @@ void	interactivemode(t_data *data, char **input)
 
 	signal(SIGINT, interactivehandle_sigint);
 	signal(SIGQUIT, interactivehandle_sigquit);
+	signal(SIGTSTP, SIG_IGN);
 	while (1)
 	{
 		info = getinfo(data);
@@ -65,6 +87,7 @@ void	noninteractivemode(t_data *data, char **input)
 {
 	signal(SIGINT, noninteractivehandle_sigint);
 	signal(SIGQUIT, noninteractivehandle_sigint);
+	signal(SIGTSTP, SIG_IGN);
 	while (data->cmd.running == 1 && g_signalint != 130)
 	{
 		if (g_signalint == 130)
